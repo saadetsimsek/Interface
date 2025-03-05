b6f020aa1122e8d868d328d9a17b1a974a2f0993
saadetsimsek <saadetsimsek13@gmail.com>
2025-03-05 23:59:59 +0300

    Initial Commit

---
+++ InterfaceTests/InterfaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceTests.swift
+//  InterfaceTests
+//
+//  Created by Saadet Şimşek on 05/03/2025.
+//
+
+import Testing
+@testable import Interface
+
+struct InterfaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
